382ab61a4e1930230bf105ff7c17200062cef36a
Artem Safin <artemsafin.webdev@gmail.com>
2021-08-03 07:53:22 +0300

    Фикс меню

--- src/js/logoMenu.js
+++ src/js/logoMenu.js
@@ -1,19 +1,30 @@
+import { primaryInput } from 'detect-it';
+
 export default function logoMenu() {
     const elements = Array.from(document.querySelectorAll('.js-logo-menu'));
 
     elements.forEach(element => {
         const btn = element.querySelector('.page-header__logo-menu-burger');
 
-        element.addEventListener('mouseenter', () => {
-            element.classList.add('active');
-        })
-        element.addEventListener('mouseleave', () => {
-            element.classList.remove('active');
-        });
+        if (primaryInput === 'touch') {
+            document.addEventListener('click', event => {
+                const insideModal = event.target.matches('.js-logo-menu') || event.target.closest('.js-logo-menu');
+                if (!insideModal) {
+                    element.classList.remove('active');
+                }
+            })
+        } else {
+            element.addEventListener('mouseenter', () => {
+                element.classList.add('active');
+            });
+            element.addEventListener('mouseleave', () => {
+                element.classList.remove('active');
+            });
+        }
 
         btn.addEventListener('click', event => {
             event.preventDefault();
             element.classList.toggle('active');
-        })
-    })
+        });
+    });
 }
